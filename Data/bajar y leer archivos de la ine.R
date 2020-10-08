a97080c53a590bc7f8b568ecd9a81356008a286b
OJP98 <ojuarez_p@hotmail.com>
2020-10-08 17:31:01 -0600

    Corrigiendo encoding y agregando .gitignor

--- Data/bajar y leer archivos de la ine.R
+++ Data/bajar y leer archivos de la ine.R
@@ -1,6 +1,6 @@
 # Oscar Juarez
 # Febrero 2019
-# Script que sirve para descargar y unir archivos de datos de veh√≠culos de la INE
+# Script que sirve para descargar y unir archivos de datos de vehÌculos de la INE
 
 #Paquetes necesarios
 #install.packages("lubridate")
@@ -15,21 +15,21 @@ library("openxlsx")
 library("dplyr")
 
 # Directorio principal
-setwd("./Data")
+setwd("./")
 
-# Para obtener los links us√© la siguiente shiny apps disponible en:https://spannbaueradam.shinyapps.io/r_regex_tester/ con el 
-# c√≥digo fuente de la p√°gina de la SAT y el siguiente patr√≥n: https://portal.sat.gob.gt/portal/descarga/5030/importacion-de-vehiculos/[[:digit:]]{4}/importacion_de_vehiculos_[[:digit:]]{4}_[[:lower:]]+.zip
+# Para obtener los links usÈ la siguiente shiny apps disponible en:https://spannbaueradam.shinyapps.io/r_regex_tester/ con el 
+# cÛdigo fuente de la p·gina de la SAT y el siguiente patrÛn: https://portal.sat.gob.gt/portal/descarga/5030/importacion-de-vehiculos/[[:digit:]]{4}/importacion_de_vehiculos_[[:digit:]]{4}_[[:lower:]]+.zip
 
 download_without_overwrite <- function(url, folder, nombreArchivo, anio)
 {
   filename <- basename(url)
   base <- tools::file_path_sans_ext(filename)
   ext <- tools::file_ext(filename)
-
+  
   file_exists <- grepl(base, list.files(folder), fixed = TRUE)
-
+  
   filename <- paste0(nombreArchivo, "_" , anio, ".", ext)
-
+  
   download.file(url, file.path(folder, filename), mode = "wb", method = "libcurl")
 }
 
@@ -67,42 +67,42 @@ links_hechos_transito<-links_hechos_transito[links_hechos_transito!=""]
 links_fallecidos_lesionados<-str_trim(unlist(strsplit(links_fallecidos_lesionados,"[[:cntrl:]]")))
 links_fallecidos_lesionados<-links_fallecidos_lesionados[links_fallecidos_lesionados!=""]
 
-# Descargar vinculos de veh√≠culos involucrados
+# Descargar vinculos de vehÌculos involucrados
 anio <- 2016
 for (vinculo in links_vehiculos_involucrados) {
-	download_without_overwrite(
-		vinculo,
-		getwd(),
-		"vehiculos_involucrados",
-		anio
-	)
-	anio <- anio + 1
+  download_without_overwrite(
+    vinculo,
+    getwd(),
+    "vehiculos_involucrados",
+    anio
+  )
+  anio <- anio + 1
   Sys.sleep(1)
 }
 
-# Descargar vinculos de hechos de tr√°nsito
+# Descargar vinculos de hechos de tr·nsito
 anio <- 2016
 for (vinculo in links_hechos_transito) {
-	download_without_overwrite(
-		vinculo,
-		getwd(),
-		"hechos_transito",
-		anio
-	)
-	anio <- anio + 1
+  download_without_overwrite(
+    vinculo,
+    getwd(),
+    "hechos_transito",
+    anio
+  )
+  anio <- anio + 1
   Sys.sleep(1)
 }
 
 # Descargar vinculos de fallecidos y lesionados
 anio <- 2016
 for (vinculo in links_fallecidos_lesionados) {
-	download_without_overwrite(
-		vinculo,
-		getwd(),
-		"fallecidos_lesionados",
-		anio
-	)
-	anio <- anio + 1
+  download_without_overwrite(
+    vinculo,
+    getwd(),
+    "fallecidos_lesionados",
+    anio
+  )
+  anio <- anio + 1
   Sys.sleep(1)
 }
 
@@ -117,23 +117,23 @@ for (archivo in listaArchivos){
   base <- tools::file_path_sans_ext(archivo)
   
   # Si es de tipo vehiculos involucrados
-	if(substr(base, 1, nchar(base)-5) == "vehiculos_involucrados"){
-	  
-	  # Se crea el df si no existe
-	  if(!exists("dfVehiculosInvolucrados"))
-	  {
-	    dfVehiculosInvolucrados <- data.frame() 
-	    dfVehiculosInvolucrados <- read.xlsx(archivo)
-	  }
-	  
-	  # Se adjuntan el resto de df's
-	  else
-	  {
-	    temp_dataset <- read.xlsx(archivo)
-	    dfVehiculosInvolucrados <- Reduce(function(...)merge (..., all=T), list(dfVehiculosInvolucrados, temp_dataset))
-	    rm(temp_dataset)
-	  }
-	}
+  if(substr(base, 1, nchar(base)-5) == "vehiculos_involucrados"){
+    
+    # Se crea el df si no existe
+    if(!exists("dfVehiculosInvolucrados"))
+    {
+      dfVehiculosInvolucrados <- data.frame() 
+      dfVehiculosInvolucrados <- read.xlsx(archivo)
+    }
+    
+    # Se adjuntan el resto de df's
+    else
+    {
+      temp_dataset <- read.xlsx(archivo)
+      dfVehiculosInvolucrados <- Reduce(function(...)merge (..., all=T), list(dfVehiculosInvolucrados, temp_dataset))
+      rm(temp_dataset)
+    }
+  }
   
   # Repetir con otros archivos 
   else if (substr(base, 1, nchar(base)-5) == "hechos_transito"){
@@ -149,7 +149,7 @@ for (archivo in listaArchivos){
       rm(temp_dataset)
     }
   }
-
+  
   else if (substr(base, 1, nchar(base)-5) == "fallecidos_lesionados"){
     if(!exists("dfFallecidosLesionados"))
     {
@@ -166,9 +166,9 @@ for (archivo in listaArchivos){
 }
 
 #Dataframes que contiene las variables que vamos a utilizar
-dfHechoTransitoLimpio <- select(dfHechoTransito,n√∫m_corre, d√≠a_ocu,a√±o_ocu,mes_ocu,d√≠a_sem_ocu,mupio_ocu,depto_ocu,tipo_veh,tipo_eve,g_hora,g_hora_5,)
-dfVehiculosInvolucradosLimpio <- select(dfVehiculosInvolucrados,n√∫m_corre, d√≠a_ocu,a√±o_ocu,mes_ocu,d√≠a_sem_ocu,mupio_ocu,depto_ocu,sexo_per,edad_per,mayor_menor,tipo_veh,tipo_veh,g_edad_80ym√°s,g_edad_60ym√°s,edad_quinquenales,g_hora,g_hora_5)
-dfFallecidosLesionadosLimpioselect <- select(dfFallecidosLesionados,n√∫m_corre,a√±o_ocu,d√≠a_ocu,g_hora,g_hora_5,mes_ocu,d√≠a_sem_ocu,mupio_ocu,depto_ocu,sexo_per,edad_per,g_edad_80ym√°s,g_edad_60ym√°s,edad_quinquenales,mayor_menor,tipo_veh,tipo_eve,fall_les,int_o_noint)
+dfHechoTransitoLimpio <- select(dfHechoTransito,n˙m_corre, dÌa_ocu,aÒo_ocu,mes_ocu,dÌa_sem_ocu,mupio_ocu,depto_ocu,tipo_veh,tipo_eve,g_hora,g_hora_5,)
+dfVehiculosInvolucradosLimpio <- select(dfVehiculosInvolucrados,n˙m_corre, dÌa_ocu,aÒo_ocu,mes_ocu,dÌa_sem_ocu,mupio_ocu,depto_ocu,sexo_per,edad_per,mayor_menor,tipo_veh,tipo_veh,g_edad_80ym·s,g_edad_60ym·s,edad_quinquenales,g_hora,g_hora_5)
+dfFallecidosLesionadosLimpioselect <- select(dfFallecidosLesionados,n˙m_corre,aÒo_ocu,dÌa_ocu,g_hora,g_hora_5,mes_ocu,dÌa_sem_ocu,mupio_ocu,depto_ocu,sexo_per,edad_per,g_edad_80ym·s,g_edad_60ym·s,edad_quinquenales,mayor_menor,tipo_veh,tipo_eve,fall_les,int_o_noint)
 
 
 names(dfFallecidosLesionadosLimpioselect)
@@ -178,5 +178,4 @@ names(dfHechoTransitoLimpio)
 write.csv(dfHechoTransitoLimpio, file="HechoTransito.csv",row.names = F)
 write.csv(dfVehiculosInvolucradosLimpio, file="VehiculosInvolucrados.csv",row.names = F)
 write.csv(dfFallecidosLesionadosLimpioselect, file="FallecidosLesionados.csv",row.names = F)
-# Salir del directorio de los .xlsx
-setwd("../../AnalisisR/")
+
